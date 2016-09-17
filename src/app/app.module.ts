500ac420032be39b5f3d17ea40d23571445d90bf
Kiattiphong Saetang <lengkubpome@hotmail.com>
2016-09-18 00:58:22 +0700

    new start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// Material2 
+import { MdButtonModule } from '@angular2-material/button';
+import { MdCardModule } from '@angular2-material/card';
+// Materialize
+import { MaterializeModule } from 'angular2-materialize';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +18,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MdButtonModule,MdCardModule,
+    MaterializeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
